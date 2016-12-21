51e0b465b4b792ac4fcb545668c611aeeabef562
Hoff Silva <hoff.henry@gmail.com>
2016-12-21 14:52:05 -0200

    Inicio do capitulo 4

---
+++ Chapter4/Chapter4.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+/*Mini-exercises
+ 
+1. Create a constant called myAge and set it to your age. Then, create a constant called isTeenager that uses Boolean logic to determine if the age denotes someone in the age range of 13 to 19.
+*/
+let myAge = 30
+let isTeenager = myAge < 19 && myAge > 13
+
+/*
+ 2. Create another constant called theirAge and set it to myage, which is 30. Then, create a constant called bothTeenagers that uses Boolean logic to determine if both you and I are teenagers.
+ */
+let theirAge = 30
+let bothTeenagers = (myAge < 19 && myAge > 13) && (theirAge < 19 && theirAge > 13)
+
+/*
+ 3. Create a constant called reader and set it to your name as a string. Create a constant called author and set it to my name, Matt Galloway. Create a constant called authorIsReader that uses string equality to determine if reader and author are equal.
+ */
+let reader  = "Hoff"
+let author = "Matt Galloway"
+let authorIsReader = reader == author
+
+/*
+ 4. Create a constant called readerBeforeAuthor which uses string comparison to determine if reader comes before author.
+ */
+let readerBeforeAuthor = reader < author
+
+
+
+
+		
