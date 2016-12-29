85a2ca6eab5b1ee89c12733d9811479155fc9b8b
Hoff Silva <hoff.henry@gmail.com>
2016-12-29 11:08:07 -0200

    Finalizacao
    Finalização do capitulo 5.

--- Chapter5/Chapter5.playground/Contents.swift
+++ Chapter5/Chapter5.playground/Contents.swift
@@ -94,5 +94,101 @@ default:
     print("Anybody have this age!")
 }
 
+/*
+ Challenges
+ */
 
+/*
+ 1. In the following for loop:
+ var sum = 0
+ for i in 0...5 {
+    sum += i 
+ }
+ What will be the value of sum, and how many iterations will happen?
+ sum = 15
+ 6 times
+ */
+var sum = 0
+for i in 0...5 {
+    sum += i
+}
+sum
+
+/*
+ 2. In the while loop below:
+ var aLotOfAs = ""
+ while aLotOfAs.characters.count < 10 {
+    aLotOfAs += "a"
+ }
+ How many instances of the character “a” will there be in aLotOfAs? Hint: aLotOfAs.characters.count will tell you how many characters there are in the string aLotOfAs.
+ 10 times
+ */
+var aLotOfAs = ""
+while aLotOfAs.characters.count < 10 {
+    aLotOfAs += "a"
+}
+
+/*
+ 3. Consider the following switch statement:
+ switch coordinates {
+    case let (x, y, z) where x == y && y == z:
+        print("x = y = z")
+    case (_, _, 0):
+        print("On the x/y plane")
+    case (_, 0, _):
+        print("On the x/z plane")
+    case (0, _, _):
+        print("On the y/z plane")
+    default:
+        print("Nothing special")
+ }
+ What will this code print when coordinates is each of the following?
+ let coordinates = (1, 5, 0) print("On the x/y plane")
+ let coordinates = (2, 2, 2) print("x = y = z")
+ let coordinates = (3, 0, 1) print("On the x/z plane")
+ let coordinates = (3, 2, 5) print("Nothing special")
+ let coordinates = (0, 2, 4) print("On the y/z plane")
+ */
+let coordinates = (1, 5, 0) //print("On the x/y plane")
+//let coordinates = (2, 2, 2) //print("x = y = z")
+//let coordinates = (3, 0, 1) //print("On the x/z plane")
+//let coordinates = (3, 2, 5) //print("Nothing special")
+//let coordinates = (0, 2, 4) //print("On the y/z plane")
+switch coordinates {
+case let (x, y, z) where x == y && y == z:
+    print("x = y = z")
+case (_, _, 0):
+    print("On the x/y plane")
+case (_, 0, _):
+    print("On the x/z plane")
+case (0, _, _):
+    print("On the y/z plane")
+default:
+    print("Nothing special")
+}
+
+/*
+ 4. A closed range can never be empty. Why?
+  because she haves a sequence of data.
+ */
+
+/*
+ 5. Print a countdown from 10 to 0. (Note: do not use the reversed() method, which will be introduced later.)
+ */
+print()
+var countdown = 10
+while countdown >= 0 {
+    print(countdown)
+    countdown -= 1
+    
+}
+/*
+ 6. Print 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0. (Note: do not use the stride(from:by:to:) function, which will be introduced later.)
+ */
+print()
+var countDouble = 0.0
+while countDouble <= 1.0 {
+    print(countDouble)
+    countDouble += 0.1
+}
 
