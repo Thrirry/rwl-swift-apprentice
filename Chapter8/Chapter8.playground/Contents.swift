7ea8f83f65eef2e9a51cd5df3e64dc892958e6c1
Hoff Silva <hoff.henry@gmail.com>
2017-01-10 12:33:27 -0200

    Evolucao

--- Chapter8/Chapter8.playground/Contents.swift
+++ Chapter8/Chapter8.playground/Contents.swift
@@ -32,7 +32,7 @@ array4[1...2]
 
 //For the final five statements, array5 has been declared as:
 
-var array5 = [1,2,3]
+var array5 = [1,2,3,4,5,6,7,8,9,10,11,12,13,15]
 array5[0] = array5[1]
 array5[0...1] = [4,5]
 //array5[0] = six erro de tipagem, um array de inteiros nao pode ter um dado do tipo string
@@ -51,5 +51,71 @@ func removingOnce(_ item: Int, from array: [Int]) -> [Int]{
 }
 removingOnce(0, from: array5)
 
-// 3 -
+// 3 - Write a function that removes all occurrences of a given integer from an array of integers. 
+var array6 = [1,2,3,3,3,4,5,6,3,3]
+func removing(_ item: Int,  from array: [Int]) -> [Int]{
+    var result : [Int] = []
+    for itemFrom in array {
+        if item != itemFrom{
+            result.append(itemFrom)
+        }
+    }
+    return result
+}
+
+removing(3, from: array6)
+
+// 4 - Write a functio that does the same thing that reversed() do.
+
+
+
+func reversed(_ array: [Int]) -> [Int]{
+    var newArray : [Int] = []
+    var array = array
+    for item in array {
+        newArray.append(array.last!)
+        array.removeLast()
+    }
+    return newArray
+}
+
+reversed(array6)
+
+// 5 - Use the function below to write a function that shuffles the elements of an array in random order
+
+func randomFromZero(to number: Int) -> Int{
+    return Int(arc4random_uniform(UInt32(number)))
+}
+
+func randomized(_ array: [Int]) -> [Int]{
+    var newArray = array
+    for item in array.enumerated() {
+        newArray[randomFromZero(to: array.count)] = item.element
+    }
+    return newArray
+}
+
+randomized(array6)
+
+// 6 - Write a func that calculates the max and min value in an array of integers. Calculate the values yourself; dont use the methods min and max. Return nil if the given array is empty
+
+func minMax(of numbers: [Int]) -> (min: Int, max: Int)?{
+    var array = numbers
+    var max = 0
+    var min = 0
+    for item in numbers {
+        for next in array.enumerated(){
+            if item > next.offset+1.{
+                max = item
+            }
+            if item < next {
+                min = item
+            }
+        }
+    }
+    
+    return (min, max)
+}
+
+minMax(of: array5)
 		
