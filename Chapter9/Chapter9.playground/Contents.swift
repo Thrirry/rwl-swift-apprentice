f8880436370afc5c727893b4a36855b4f46b39b5
Hoff Silva <hoff.henry@gmail.com>
2017-01-11 12:44:47 -0200

    Sas

--- Chapter9/Chapter9.playground/Contents.swift
+++ Chapter9/Chapter9.playground/Contents.swift
@@ -3,4 +3,44 @@
 import UIKit
 import Foundation
 
-var str = "Hello, playground"
+var namesAndScores = ["Anna" : 2, "Brian" : 2, "Craig" : 8, "anna" : 3, "Donna" : 6 ] //String : Int
+var emptyDictionary : [String: Int] = [:]
+print()
+
+var hoff = ["nome" : "Hoff Silva", "celular" : 61991212700, "cidade" : "Brasilia", "estado" : "Distrito Federal", "pais" : "Brasil"] as [String : Any]
+var mariana = ["nome" : "Mariana Camara", "celular" : 61992756914, "cidade" : "Brasilia", "estado" : "Distrito Federal", "pais" : "Brasil"] as [String : Any]
+
+var name = "Anna"
+if let score = namesAndScores[name] {
+    print(score)
+}else{
+    print("The \(name) isn't a dictionary key.")
+}
+
+namesAndScores.isEmpty
+namesAndScores.count
+for item in namesAndScores.enumerated() {
+  print(item.offset)
+}
+Array(namesAndScores.keys)
+Array(namesAndScores.values)
+
+namesAndScores.updateValue(4, forKey: "Anna")
+namesAndScores[name]
+
+func printUserData(_ user: [String : Any] ){
+    if let cidade = user["cidade"] {
+        print(cidade)
+    }
+    if let estado = user["estado"] {
+        print(estado)
+    }
+}
+printUserData(mariana)
+
+mariana["estado"] = "DF"
+
+printUserData(mariana)
+
+//Challenges
+
